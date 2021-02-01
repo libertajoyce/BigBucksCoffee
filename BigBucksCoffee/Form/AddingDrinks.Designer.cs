cba404f1241fed93b205798a9e8c768835a38403
DotEmms <emmadanckaert@hotmail.com>
2021-02-01 17:02:48 +0100

    altered buttons (design) of Add drink

--- BigBucksCoffee/Form/AddingDrinks.Designer.cs
+++ BigBucksCoffee/Form/AddingDrinks.Designer.cs
@@ -34,6 +34,7 @@ namespace BigBucksCoffee
             this.rbSoda = new System.Windows.Forms.RadioButton();
             this.rbSmoothie = new System.Windows.Forms.RadioButton();
             this.gbDrinkChoices = new System.Windows.Forms.GroupBox();
+            this.btnConfirm = new System.Windows.Forms.Button();
             this.lblAddingName = new System.Windows.Forms.Label();
             this.lblAddingPrice = new System.Windows.Forms.Label();
             this.lblAddingDescription = new System.Windows.Forms.Label();
@@ -48,7 +49,6 @@ namespace BigBucksCoffee
             this.ofdImageSelector = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.button1 = new System.Windows.Forms.Button();
-            this.btnConfirm = new System.Windows.Forms.Button();
             this.gbDrinkChoices.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numAddingPrice)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbAddingPicture)).BeginInit();
@@ -100,7 +100,6 @@ namespace BigBucksCoffee
             // 
             // gbDrinkChoices
             // 
-            this.gbDrinkChoices.Controls.Add(this.btnConfirm);
             this.gbDrinkChoices.Controls.Add(this.rbCoffee);
             this.gbDrinkChoices.Controls.Add(this.rbSmoothie);
             this.gbDrinkChoices.Controls.Add(this.rbTea);
@@ -112,6 +111,16 @@ namespace BigBucksCoffee
             this.gbDrinkChoices.TabIndex = 4;
             this.gbDrinkChoices.TabStop = false;
             this.gbDrinkChoices.Text = "Drinks";
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(310, 491);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(95, 29);
+            this.btnConfirm.TabIndex = 4;
+            this.btnConfirm.Text = "Confirm";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
             // 
             // lblAddingName
             // 
@@ -168,7 +177,7 @@ namespace BigBucksCoffee
             this.txtAddingName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtAddingName.Location = new System.Drawing.Point(12, 200);
             this.txtAddingName.Name = "txtAddingName";
-            this.txtAddingName.Size = new System.Drawing.Size(231, 34);
+            this.txtAddingName.Size = new System.Drawing.Size(231, 27);
             this.txtAddingName.TabIndex = 10;
             // 
             // txtAddingDescription
@@ -229,29 +238,20 @@ namespace BigBucksCoffee
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(314, 491);
+            this.button1.Location = new System.Drawing.Point(419, 491);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(194, 29);
+            this.button1.Size = new System.Drawing.Size(93, 29);
             this.button1.TabIndex = 17;
-            this.button1.Text = "Save ";
+            this.button1.Text = "Save to file";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // btnConfirm
-            // 
-            this.btnConfirm.Location = new System.Drawing.Point(239, 85);
-            this.btnConfirm.Name = "btnConfirm";
-            this.btnConfirm.Size = new System.Drawing.Size(103, 51);
-            this.btnConfirm.TabIndex = 4;
-            this.btnConfirm.Text = "Confirm";
-            this.btnConfirm.UseVisualStyleBackColor = true;
-            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
             // 
             // AddingDrinks
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(851, 553);
+            this.Controls.Add(this.btnConfirm);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnSelectImage);
             this.Controls.Add(this.pbAddingPicture);
